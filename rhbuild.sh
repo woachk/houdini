1eb3176b4124a7555ec6db1163ce0a794d3a6b5d
woachk <24752637+woachk@users.noreply.github.com>
2018-11-21 17:33:08 +0100

    Fix RH build script

--- rhbuild.sh
+++ rhbuild.sh
@@ -2,7 +2,7 @@
 mkdir -p houdini-7
 cp -r *.sfs houdini.conf build.sh install64.sh patch.sh houdini-7/
 tar cvf houdini-7.tar houdini-7
-cp houdini-7.tar $HOME/rpmbuild/SOURCES/houdini-6.tar
+cp houdini-7.tar $HOME/rpmbuild/SOURCES/houdini-7.tar
 rpmbuild -bb houdini.spec
 rm -rf houdini-7
 rm -rf houdini-7.tar
