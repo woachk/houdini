b47fa8cd147bf1af15bc9385b0d267ec806017e0
woachk <24752637+woachk@users.noreply.github.com>
2020-04-28 18:04:27 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -6,25 +6,38 @@
 ### Houdini 6
 #
 
-# Houdini 6 - ARMv8 on x86_64
+# Houdini 6 - AArch64 on x86_64
 wget -O houdini-6_z.sfs http://dl.android-x86.org/houdini/6_z/houdini.sfs 
 
-# Houdini 6 - ARMv7 on x86_64
+# Houdini 6 - AArch32 on x86_64
 wget -O houdini-6_y.sfs http://dl.android-x86.org/houdini/6_y/houdini.sfs
 
-# Houdini 6 - ARMv7 on x86_32
+# Houdini 6 - AArch32 on x86_32
 wget -O houdini-6_x.sfs http://dl.android-x86.org/houdini/6_x/houdini.sfs
 
 #
 ### Houdini 7
 #
 
-# Houdini 7 - ARMv8 on x86_64
+# Houdini 7 - AArch64 on x86_64
 wget -O houdini-7_z.sfs http://dl.android-x86.org/houdini/7_z/houdini.sfs 
 
-# Houdini 7 - ARMv7 on x86_64
+# Houdini 7 - AArch32 on x86_64
 wget -O houdini-7_y.sfs http://dl.android-x86.org/houdini/7_y/houdini.sfs
 
-# Houdini 7 - ARMv7 on x86_32
+# Houdini 7 - AArch32 on x86_32
 wget -O houdini-7_x.sfs http://dl.android-x86.org/houdini/7_x/houdini.sfs
 
+#
+### Houdini 8
+#
+
+# Houdini 8 - AArch32 on x86_64
+wget -O houdini-8_y.sfs http://dl.android-x86.org/houdini/8_y/houdini.sfs
+
+#
+### Houdini 9
+#
+
+# Houdini 9 - AArch32 on x86_64
+wget -O houdini-9_y.sfs http://dl.android-x86.org/houdini/9_y/houdini.sfs
